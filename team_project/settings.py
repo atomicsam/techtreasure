56638ddcf936d7fac32f544f6191fd751fbd22c4
Sam Saju <2618654s@student.gla.ac.uk>
2024-03-04 10:57:27 +0000

    Added some static files and implemented halfway up to chapter 4 of the rango textbook

--- team_project/settings.py
+++ team_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'techtreasure',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
