ec840f2125cb7a5f2220c9f3fd852f19605dd605
Sam Saju <2618654s@student.gla.ac.uk>
2024-03-07 23:55:36 +0000

    Removed some comments

--- techtreasure/forms.py
+++ techtreasure/forms.py
@@ -25,11 +25,4 @@ class PageForm(forms.ModelForm):
         # Provide an association between the ModelForm and a model
         model = Listing
 
-        # What fields do we want to include in our form?
-        # This way we don't need every field in the model present.
-        # Some fields may allow NULL values; we may not want to include them.
-        # Here, we are hiding the foreign key.
-        # we can either exclude the category field from the form,
         exclude = ('category',)
-        # or specify the fields to include (don't include the category field).
-        #fields = ('title', 'url', 'views')
