a88e0d29c9f68e84eef7fd6240171b1adcc3cc70
Sam Saju <2618654s@student.gla.ac.uk>
2024-03-12 19:39:52 +0000

    Fixed Cards & added some animations

--- techtreasure/templatetags/techtreasure_template_tags.py
+++ techtreasure/templatetags/techtreasure_template_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from techtreasure.models import Category
+from techtreasure.models import Category, Listing
 
 register = template.Library()
 
@@ -8,8 +8,8 @@ def get_category_list(categories=Category.objects.all()):
     return {'categories': categories}
 
 @register.inclusion_tag('techtreasure/list_listings.html')
-def get_listings_list(categories=Category.objects.all()):
-    return {'categories': categories}
+def get_listings_list(listings=Listing.objects.all()):
+    return {'listings': listings}
 
 @register.inclusion_tag('techtreasure/navbar_dropdown.html')
 def get_all_categories():
